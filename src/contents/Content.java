ba2cec8126630980e0bf3b2f833b39899eee45e3
ILMaverick <pallottimatteo@gmail.com>
2024-02-19 19:59:47 +0100

    creati Contents e Elements

---
+++ src/contents/Content.java
@@ -0,0 +1,27 @@
+package contents;
+
+public abstract class Content {
+
+	private boolean status;
+
+	public void mofify() {
+		// TODO
+	}
+
+	public void create() {
+		// TODO
+	}
+
+	public void show() {
+		// TODO
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+}
