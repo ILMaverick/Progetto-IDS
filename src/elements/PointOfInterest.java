c4884196f00af500dc89751980a7f5254b46714d
SilverSimon-xyz <153428740+SilverSimon-xyz@users.noreply.github.com>
2024-03-01 22:42:49 +0100

    Update PointOfInterest.java

--- src/elements/PointOfInterest.java
+++ src/elements/PointOfInterest.java
@@ -3,7 +3,38 @@ package elements;
 public class PointOfInterest {
 
 	private String name;
-	private int ID;
-	private Coordinates coordinates;
-	
+	private final int ID;
+	private final Coordinates coordinates;
+	private ContentAlbum album;
+
+	public PointOfInterest(String name, int ID, Coordinates coordinates, ContentAlbum album) {
+		this.name = name;
+		this.ID = ID;
+		this.coordinates = coordinates;
+		this.album = album;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public Coordinates getCoordinates() {
+		return coordinates;
+	}
+
+	public ContentAlbum getContentAlbum() {
+		return album;
+	}
+
+	public void setContentAlbum(String album) {
+		this.album = album;
+	}
 }
