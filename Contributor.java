e6abb9072941f17ab9c24ee0fecee320e315b654
ASVP_Croc <lorenzo.crovace@studenti.unicam.it>
2024-03-18 12:22:20 +0100

    Contributor Class
    Definizione di Contributor

---
+++ Contributor.java
@@ -0,0 +1,34 @@
+public class Contributor extends AbstractUser {
+
+	public Contributor(String userName, String password) {
+		super(userName, password);
+	}
+
+	public void createPOI() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void createTour() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void loadContent() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void modify() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void search() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void access() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void show() {
+		throw new UnsupportedOperationException();
+	}
+}
