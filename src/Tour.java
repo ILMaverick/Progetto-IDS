31aa6fd2d6763f4a8966a6c7a22c913d21ef5509
ASVP_Croc <lorenzo.crovace@studenti.unicam.it>
2024-04-18 00:14:22 +0200

    Update Tour.java
    update

--- src/Tour.java
+++ src/Tour.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Tour {
 	private static int count=0;
 	private final int id;
 	private List<PointOfInterest> listOfPOI = new ArrayList<>();
-	private List<Content> description = new ArrayList<>();
-	public Town myTown;
 
-	public Tour(Town town){
+	public Tour(){
 		this.id=count++;
-		this.myTown=town;
 	}
 
 	public void addPOI(PointOfInterest pointOfInterest) {
@@ -21,30 +19,10 @@ public class Tour {
 		}
 	}
 
-	public void addDescription(Content content){
-		if(description.contains(content)){
-			throw new IllegalArgumentException();
-		} else description.add(content);
-	}
-
 	public void removePOI(PointOfInterest pointOfInterest){
 		if(listOfPOI.contains(pointOfInterest)){
 			listOfPOI.remove(pointOfInterest);
 		} else throw new IllegalArgumentException();
-	}
-
-	public void removeDescription(Content content){
-		if(description.contains(content)){
-			description.remove(content);
-		} else throw new IllegalArgumentException();
-	}
-
-	public Town getTown(){
-		return myTown;
-	}
-
-	public List<Content> getDescriptions(){
-		return description;
 	}
 
 	public List<PointOfInterest> getPOIs(){
